d84c9a5a294d8d0abe04b10c0fd82e15ae52f757
James <consultarchie@gmail.com>
2014-12-03 20:09:10 -0800

    Plot 4

--- plot4.R
+++ plot4.R
@@ -8,3 +8,4 @@ lines(power1$time1,power1$Sub_metering_3,type="l",col="blue")
 legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"), bty='n', cex=.75)
 plot(power1$time1,power1$Global_reactive_power,ylab="Global_reactive_power", xlab="datetime",type="l")
 dev.off()
+# the end
